c61af233d74dc3bdfcc5bf1d4b6eb70e2771a06f
Max Koretskyi <m.koretskyi@gmail.com>
2020-05-12 21:17:02 +0300

    Implemented functionality to read paths from tsconfig

--- libs/gatsby-plugin/plugins/nx-gatsby-ext-plugin/gatsby-node.ts
+++ libs/gatsby-plugin/plugins/nx-gatsby-ext-plugin/gatsby-node.ts
@@ -1,17 +1,26 @@
-const path = require('path');
+import * as path from 'path';
+import { appRootPath as workspaceRoot } from '@nrwl/workspace/src/utils/app-root';
+import { readJsonFile } from '@nrwl/workspace';
 
 function onCreateBabelConfig({ actions }, options) {
-  const libsDir = path.resolve(options.path, '../..', 'libs');
+  const tsConfig = readJsonFile(path.join(workspaceRoot, 'tsconfig.json'));
+  const tsConfigPaths: { [key: string]: Array<string> } = tsConfig.compilerOptions.paths;
+
+  const paths = Object.entries(tsConfigPaths)
+    .reduce((result, [key, paths]) => {
+      return {
+        ...result,
+        [key]: paths.map((p) => path.join(workspaceRoot, p))
+      };
+    }, {});
 
   actions.setBabelPlugin({
     name: require.resolve(`babel-plugin-module-resolver`),
     options: {
       'root': ['./src'],
-      'alias': {
-        'libs': libsDir
-      }
+      'alias': paths
     }
   });
 }
 
-exports.onCreateBabelConfig = onCreateBabelConfig;
+export { onCreateBabelConfig };
